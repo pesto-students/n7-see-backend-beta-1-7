3f3fb2942357b978b332777a8747d784eafb9aff
Kiran <Kiran@gitlab.keralapolice.gov.in>
2021-09-20 10:08:08 +0530

    push with changes

--- routers/users.js
+++ routers/users.js
@@ -8,7 +8,7 @@ router.get('/',async (req,res)=>{
     }catch(err){
         res.send()
     }
-    res.send('Get Requests');
+    // res.send('Get Requests');
 })
 
 router.get('/:id',async (req,res)=>{
@@ -22,22 +22,63 @@ router.get('/:id',async (req,res)=>{
 })
 
 router.post('/',async(req,res)=>{
-    const user=new User({
-        u_id:req.body.u_id,
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        email:req.body.email,
-        password:req.body.password,
-        usertype:req.body.usertype
-    })
-    console.log(user)
+
     try{
+        const user=new User({
+            u_id:req.body.u_id,
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            email:req.body.email,
+            password:req.body.password,
+            usertype:req.body.usertype
+        });
+        console.log(user)
         const data=await user.save() 
-        res.json(data)
+        res.status(200).json({
+            statuscode:"200",
+            response:data,
+            message:"User Registered Successfully..Please Login"
+        });
     }catch(err){
-        res.send('Error')
+        res.send()
     }
 })
+
+
+router.post('/login', async (req, res, next) =>{
+	console.log(req.body);
+    try{
+        User.findOne({email:req.body.email},function(err,data){
+            if(data){
+                if(data.password==req.body.password){
+                    //console.log("Done Login");
+                    // req.session.userId = data.u_id;
+                    res.status(200).json({
+                        statuscode:"200",
+                        response:data,
+                        message:"User Logged In Successfully"
+                    });
+                }else{
+                    res.status(401).json({
+                        statuscode:"500",
+                        response:"",
+                        message:"User Password Mismatch"
+                    });
+                }
+            }else{
+                res.status(401).json({
+                    statuscode:"500",
+                    response:"",
+                    message:"User Not Exist"
+                });
+            }
+        });
+    }catch(err){
+        res.send()
+    }
+
+});
+
 
 
 router.patch('/:id',async (req,res)=>{
