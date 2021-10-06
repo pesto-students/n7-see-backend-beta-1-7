6df662bad7521eb3ac900b2fcdb8282293f47f49
Kiran <Kiran@gitlab.keralapolice.gov.in>
2021-10-06 10:22:03 +0530

    push with env

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/admin',adminRouter);
 
 
 app.get('/',function(req,res){
-    res.send("hyy555dfsd");
+    res.send("listening to 4000");
 })
 
 app.listen(process.env.PORT || 4000,function(){
